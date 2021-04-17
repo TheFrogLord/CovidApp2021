b74ccce55ab0d84c28bf787807946099bee0f456
TheFrogLord <lwkedzierski@gmail.com>
2021-04-17 12:14:09 +0200

    CovidApp

--- src/main/java/sda/covidapp/api/HelloWorldController.java
+++ src/main/java/sda/covidapp/api/HelloWorldController.java
@@ -1,14 +1,24 @@
 package sda.covidapp.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
-    @GetMapping("/hello")
-    public String helloWorldInfo() {
-        return "Hello World";
+    @GetMapping("/hello/{langCode}")
+    public String helloWorldInfo(@PathVariable String langCode) {
+        switch (langCode.toUpperCase()) {
+            case "EN":
+                return "Hello World";
+            case "ES":
+                return "Hola";
+            case "PL":
+                return "Witaj świecie";
+            default:
+                return "Unknown lang";
+        }
     }
 
 
