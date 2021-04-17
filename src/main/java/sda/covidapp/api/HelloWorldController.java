80a28bd91c166787102730339ce6b6e7a53bed22
TheFrogLord <lwkedzierski@gmail.com>
2021-04-17 13:58:01 +0200

    UserEndCommit

--- src/main/java/sda/covidapp/api/HelloWorldController.java
+++ src/main/java/sda/covidapp/api/HelloWorldController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import sda.covidapp.api.model.User;
 
 @RestController
 public class HelloWorldController {
